a6731730950c8e41e21b7b374bd8fc56ecc0dc94
villar88 <villar88@gmail.com>
2023-03-20 15:44:32 -0400

    slider

---
+++ database/factories/TeamUserFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TeamUser>
+ */
+class TeamUserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'id_name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'email' =>  $this->faker->email(),
+            'skills' => $this->faker->text(),
+            'status' => rand(0,1),
+            'age' => rand(18,30),
+            'address' => $this->faker->text(),
+            'team_presentation' => $this->faker->text(),
+            'cv_link' => $this->faker->text(),
+            'residence' => $this->faker->text(), 
+            'freelance' => $this->faker->text(), 
+            'team_category' => 'PHP-FULL-STACK',
+            'work_time' => rand(10,40),
+            'overview' => $this->faker->text(),
+            'created_at' => $this->faker->date(),
+            'updated_at' => $this->faker->date(),
+            'photo' => 'images/'.$this->faker->file(public_path('images'), storage_path('app/public/uploads/images/'), false),
+
+        ];
+    }
+}
