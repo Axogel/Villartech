228a1e339e16ba49f26865f7f6f4a647527fa9e1
Steffani Castro <steffani.castro.useche@gmail.com>
2023-04-25 10:41:33 -0400

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
+        $this->call([
+            UsersTableSeeder::class,
+            TeamUserSeeder::class,
+            TagSeeder::class
+        ]);
     }
 }
