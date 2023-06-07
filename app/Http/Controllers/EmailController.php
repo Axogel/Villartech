529f5af868e3dd866a1a787dda6a7905948c0460
emilyperezz <emilyvpp22@gmail.com>
2023-06-07 10:06:08 -0500

    email changes

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -1,16 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Support\Facades\Mail;
-
 use Illuminate\Http\Request;
-
 use App\Models\Contact;
 use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
-
-
 
 class EmailController extends Controller
 {
@@ -18,53 +13,64 @@ class EmailController extends Controller
     public function send(Request $request)
     {
 
-        $email = $request->input('email');
-        $interest = $request->input('interest');
-        $to = 'unop0203@gmail.com';
+        {
 
-
-
-        $validator = Validator::make($request->all(), [
-            'number' => ['required', 'regex:/^[0-9\W]+$/'],
-            'budget' => ['nullable', 'regex:/^[0-9\W]+$/'],
-        ]);
+            $email = $request->input('email');
+            $interest = $request->input('interest');
+            $to = 'infovillartech@gmail.com';
     
-        if ($validator->fails()) {
-
-            Alert::error('Error', 'Check the data and try again.');
-            return redirect('/contactUs');
-        }
-
-        else {
-            Mail::send('emailClient', $request->all(), function($msg) use ($interest,$to,$email)  {
-
-            $msg->from($email);
-            $msg->subject($interest);
-            $msg->to($to);
     
-            });
+    
+            $validator = Validator::make($request->all(), [
+                'number' => ['required', 'regex:/^[0-9\W]+$/'],
+                'budget' => ['nullable', 'regex:/^[0-9\W]+$/'],
+            ]);
+        
+            if ($validator->fails()) {
+    
+                Alert::error('Error', 'Check the data and try again.');
+                return redirect('/contactUs');
+            }
+    
+            else {
+                Mail::send('email', $request->all(), function($msg) use ($interest,$to,$email)  {
+    
+                $msg->from($email);
+                $msg->subject($interest);
+                $msg->to($to);
+        
+                });
 
-            Mail::send('email', $request->all(), function($msg) use ($interest,$to,$email)  {
+
+
+                Mail::send('emailClient', $request->all(), function($msg) use ($interest,$to,$email)  {
     
                     $msg->from('infovillartech@gmail.com');
                     $msg->subject('Contact Email and Information');
                     $msg->to($email);
             
                     });
+        
+                $contact = new Contact;
+                $contact->name = $request->input('name');
+                $contact->email = $request->input('email');
+                $contact->number = $request->input('number');
+                $contact->interest = $request->input('interest');
+                $contact->message = $request->input('mensaje');
+                $contact->save();
     
-            $contact = new Contact;
-            $contact->name = $request->input('name');
-            $contact->email = $request->input('email');
-            $contact->number = $request->input('number');
-            $contact->interest = $request->input('interest');
-            $contact->message = $request->input('mensaje');
-            $contact->save();
-
-            Alert::success('Success', 'Email sended Succesfully!');
-            return redirect('/contactUs');
-
-        }
+                Alert::success('Success', 'Email sended Succesfully!');
+                return redirect('/contactUs');
+    
+            }
 
        
     }
 }
+
+
+   
+}
+
+
+
