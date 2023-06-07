fe135c171060c5b5205d6726592819e5d958edbc
Jose Villar <villar88@gmail.com>
2023-06-07 19:35:46 +0000

    try

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -103,10 +103,6 @@ class FrontController extends Controller
     {
 
         $portfolios = Portfolio::select('id', 'name', 'image', 'url', 'skills','slug', 'description')
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/develop
                                ->get();
         
         $team = TeamUser::select('*')
