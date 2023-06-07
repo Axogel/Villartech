56d453c0ba09b167e2146af8cfe378ae3680e589
Jose Villar <villar88@gmail.com>
2023-06-07 19:15:24 +0000

    fix conficts

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -1,16 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Support\Facades\Mail;
-
 use Illuminate\Http\Request;
-
 use App\Models\Contact;
 use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
-
-
 
 class EmailController extends Controller
 {
@@ -18,44 +13,53 @@ class EmailController extends Controller
     public function send(Request $request)
     {
 
-        $email = $request->input('email');
-        $interest = $request->input('interest');
-        $to = 'unop0203@gmail.com';
+        {
 
-
-
-        $validator = Validator::make($request->all(), [
-            'number' => ['required', 'regex:/^[0-9\W]+$/'],
-            'budget' => ['nullable', 'regex:/^[0-9\W]+$/'],
-        ]);
+            $email = $request->input('email');
+            $interest = $request->input('interest');
+            $to = 'infovillartech@gmail.com';
     
-        if ($validator->fails()) {
-
-            Alert::error('Error', 'Check the data and try again.');
-            return redirect('/contactUs');
-        }
-
-        else {
-            Mail::send('emailClient', $request->all(), function($msg) use ($interest,$to,$email)  {
-
-            $msg->from($email);
-            $msg->subject($interest);
-            $msg->to($to);
     
-            });
+    
+            $validator = Validator::make($request->all(), [
+                'number' => ['required', 'regex:/^[0-9\W]+$/'],
+                'budget' => ['nullable', 'regex:/^[0-9\W]+$/'],
+            ]);
+        
+            if ($validator->fails()) {
+    
+                Alert::error('Error', 'Check the data and try again.');
+                return redirect('/contactUs');
+            }
+    
+            else {
+                Mail::send('email', $request->all(), function($msg) use ($interest,$to,$email)  {
+    
+                $msg->from($email);
+                $msg->subject($interest);
+                $msg->to($to);
+        
+                });
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
             Mail::send('emailClient', $request->all(), function($msg) use ($interest,$to,$email)  {
 =======
             Mail::send('email', $request->all(), function($msg) use ($interest,$to,$email)  {
 >>>>>>> 6af7f45 (test)
+=======
+
+
+                Mail::send('emailClient', $request->all(), function($msg) use ($interest,$to,$email)  {
+>>>>>>> 529f5af (email changes)
     
                     $msg->from('infovillartech@gmail.com');
                     $msg->subject('Contact Email and Information');
                     $msg->to($email);
             
                     });
+<<<<<<< HEAD
 =======
 
             Mail::send('emailClient', $request->all(), function($msg) use ($interest,$to,$email)  {
@@ -66,20 +70,29 @@ class EmailController extends Controller
         
                 });
 >>>>>>> 18d31a0 (email test)
+=======
+        
+                $contact = new Contact;
+                $contact->name = $request->input('name');
+                $contact->email = $request->input('email');
+                $contact->number = $request->input('number');
+                $contact->interest = $request->input('interest');
+                $contact->message = $request->input('mensaje');
+                $contact->save();
+>>>>>>> 529f5af (email changes)
     
-            $contact = new Contact;
-            $contact->name = $request->input('name');
-            $contact->email = $request->input('email');
-            $contact->number = $request->input('number');
-            $contact->interest = $request->input('interest');
-            $contact->message = $request->input('mensaje');
-            $contact->save();
-
-            Alert::success('Success', 'Email sended Succesfully!');
-            return redirect('/contactUs');
-
-        }
+                Alert::success('Success', 'Email sended Succesfully!');
+                return redirect('/contactUs');
+    
+            }
 
        
     }
 }
+
+
+   
+}
+
+
+
--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -10,12 +10,12 @@
                     margin-left: auto!important;     padding-top: 2.5rem !important; display:block;">
                     <br>
             </div>
-            <div class="col-12">
-                <p class="mx-auto thank-you pt-5" style="  text-align: center; font-size: 25px; font-family: 'Lato';color: #045A97; font-weight: 900;">
-                    Thank you for contacting us!
-                </p>
-                <div class="circle" id="hola-como-vas2" style="    width: 203px;
-                height: 176px;
+            <div class="col-12" style="margin-top:10rem;">
+
+
+                <div class="circle" id="hola-como-vas2" style="
+                width: 160px;
+height: 160px;
                 background-color: #005397;
                 border-radius: 50%;
                 margin-bottom: 10px;
@@ -24,29 +24,25 @@
 
                     <img src="https://staging.villartechnologies.com.ve/images/layer1.png" alt="" style=" width: 138px;
                     height: 125px;
-                    padding-left: 36px;
-                    padding-top: 32px;
+                    padding-left: 26px;
+                    padding-top: 35px;
                 ">
                 </div>
                 <p style="font-size: 25px; color: black; font-weight: 900; text-align: center;  padding-top: 3rem!important; margin-left:auto !important; margin-right:auto !important;">
-                    Dear Test
+                    New Contact Inquiry <br> Received
                 </p>
 
-                <hr style="color:black; width:25%; text-align:center; opacity:1;" class="mx-auto">
+                <hr style="color:black; width:25%; text-align:center; opacity:o.5px;" class="mx-auto">
                 <div class="" style="margin-left:auto !important; margin-right:auto !important;     flex: 0 0 auto;
-                width: 33.33333333%;
-            ">
+                width: 33.33333333%;">
+                <br>
                     <p class="thanks" style="margin-left:auto !important; margin-right:auto !important;   font-size: 17px;
                     font-weight: 500;
                     text-align: center">
-                        Thank you for reaching out Villartechnologies. <span style="font-weight: 900;">We have recieved</span> <span style="font-weight: 900;">your message and aprecciate your interest in our services</span>
+                        A new contact inquiry has been received through the website form. Below are the details provided by the user:
                     </p>
 
-                    <p class="thanks" style="font-weight: 500;  font-size: 17px;
-                    font-weight: 500;
-                    text-align: center">
-                        Our team will review your inquiry and get back to you as soon as possible. In the meantime, if you have any additional questions or need further information, please feel free to contact us. 
-                    </p>
+
 
                     <p class="thanks" style="font-weight: 500;  font-size: 17px;
                     font-weight: 500;
